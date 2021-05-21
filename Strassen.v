cd2a6a47495e0e751eb0650766ab5f79b6c36434
TaoYC0904 <taoyc0904@sjtu.edu.cn>
2021-05-21 14:27:18 +0800

    Update Strassen as a quadrable relation

--- Strassen.v
+++ Strassen.v
@@ -7,26 +7,26 @@ Require Import ZSum.
 Require Export Coq.ZArith.ZArith.
 Require Import Matrix.
 
-Inductive StrassenMult {n : nat} : Square n -> Square n -> Square n -> Prop :=
-  | SM_1 : forall (A B C : Square n), 
-      n = 1 -> (A 0 0) * (B 0 0) = (C 0 0) ->
-      StrassenMult A B C.
-  | SM_n : forall (A B C : Square 2 * n), 
+
+Open Scope matrix_scope.
+
+Search (Z -> nat). (* Z.to_nat *)
+Search (nat -> Z). (* Z.of_nat *)
+
+
+
+Inductive StrassenMult: 
+  nat -> Square  -> Square -> Square -> Prop :=
+  | SM_1 : forall (n : nat) (A B C : Square n), 
+      n = Z.to_nat 1 -> C = A × B ->
+      StrassenMult n A B C.
+  | SM_n : forall (A B C : Square (2 * n))
                   (A11 A12 A21 A22 B11 B12 B21 B22 C11 C12 C21 C22
                   S1 S2 S3 S4 S5 S6 S7 S8 S9 S10
                   P1 P2 P3 P4 P5 P6 P7 : Square n),
-      A11 = subMat A 0 0 ->
-      A12 = subMat A 0 n/2 ->
-      A21 = subMat A n/2 0 -> 
-      A22 = subMat A n/2 n/2 ->
-      B11 = subMat B 0 0 ->
-      B12 = subMat B 0 n/2 ->
-      B21 = subMat B n/2 0 -> 
-      B22 = subMat B n/2 n/2 ->
-      C11 = subMat C 0 0 ->
-      C12 = subMat C 0 n/2 ->
-      C21 = subMat C n/2 0 -> 
-      C22 = subMat C n/2 n/2 ->
+      Split A A11 A12 A21 A22 ->
+      Split B B11 B12 B21 B22 ->
+      Split C C11 C12 C21 C22 ->
       S1 = B12 - B22 ->
       S2 = A11 + A12 ->
       S3 = A21 + A22 -> 
@@ -52,4 +52,6 @@ Inductive StrassenMult {n : nat} : Square n -> Square n -> Square n -> Prop :=
 
 
 Theorem StrassenCorrectness {n : nat} : 
-  forall (A B C : Square n), StrassenMult A B C -> C = A * B.
+  forall (A B C : Square n), @StrassenMult n A B C -> C = A × B.
+
+  
